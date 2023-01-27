b6dd3cbea829fba7fa9eb84303097b1820c8add8
leinworld <leinworld@gmail.com>
2023-01-27 17:45:48 +0900

    SSL applied, working around

--- devenv/entrypoint.sh
+++ devenv/entrypoint.sh
@@ -2,6 +2,18 @@
 set -e
 
 useradd -g $PGID -u $PUID -m --shell /bin/bash $USERNAME
+echo "$USERNAME:$PASSWD" | chpasswd
+
+/usr/sbin/sshd
+
+mkdir /certs
+mkcert --install
+mkcert \
+ -cert-file /certs/local-cert.pem \
+ -key-file /certs/local-key.pem \
+ 192.168.0.101 leinworld 127.0.0.1 localhost
+
+chown -R $PUID:$PGID /certs
 
 export HOME=/home/$USERNAME
 export XDG_CONFIG_HOME=$HOME/.config
