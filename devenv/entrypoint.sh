80ee8477b2d756653f5ca9d6424d7efe46cecd77
leinworld <leinworld@gmail.com>
2023-02-02 01:07:04 +0900

    Add npm Make re-start without remove image

--- devenv/entrypoint.sh
+++ devenv/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+if ! getent passwd $USERNAME > /dev/null
+then
 useradd -g $PGID -u $PUID -m --shell /bin/bash $USERNAME
 echo "$USERNAME:$PASSWD" | chpasswd
+fi
 
 /usr/sbin/sshd
 
+if [ ! -d /certs ]
+then
 mkdir /certs
 mkcert --install
 mkcert \
@@ -14,6 +19,7 @@ mkcert \
  192.168.0.101 leinworld 127.0.0.1 localhost
 
 chown -R $PUID:$PGID /certs
+fi
 
 export HOME=/home/$USERNAME
 export XDG_CONFIG_HOME=$HOME/.config
